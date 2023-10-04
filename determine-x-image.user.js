3c9698d74d8f957e81846b53fc4facad0c083a54
Ayane <me@ayane.io>
2023-10-04 19:17:14 +0800

    chore: bump version to `1.2`

--- determine-x-image.user.js
+++ determine-x-image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Determine X Image
 // @namespace    atk-def
-// @version      1.1
+// @version      1.2
 // @description  Retrieve the original image dimensions and download request in X (Twitter).
 // @author       Ayane
 // @match        https://x.com/*
